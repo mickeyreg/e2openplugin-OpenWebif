28ad30194f8a5ade8602e9c8d0eebd9b6a590095
mickeyreg <mickeyreg@gmail.com>
2014-12-04 09:28:03 +0100

    A typo...

--- plugin/controllers/models/info.py
+++ plugin/controllers/models/info.py
@@ -141,17 +141,10 @@ def getInfo():
 	# TODO: get webif versione somewhere!
 	info = {}
 
-<<<<<<< HEAD
 	info['brand'] = getMachineBrand()
 	info['model'] = getMachineName()
 	info['boxtype'] = getBoxType()
 	info['machinebuild'] = getMachineBuild()
-=======
-	brand = "unknown"
-	model = "unknown"
-	model_desc = "unknown"
-	chipset = "unknown"
->>>>>>> 1390f50... Update for GOS
 
 	chipset = "unknown"
 	if fileExists("/proc/stb/info/model"):
